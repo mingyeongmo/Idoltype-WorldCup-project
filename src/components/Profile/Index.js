9ff6275f9fa660f3a657179ac5544aacf5c328cb
mingyeongmo <mingyeongmp@naver.com>
2021-07-12 22:39:21 +0900

    Image array plus

--- src/components/Profile/Index.js
+++ src/components/Profile/Index.js
@@ -15,6 +15,22 @@ const items = [
         name:"채령",
         src:require("../../Assets/itzyimgs/chaeryeong1.svg")
     },
+    {
+        name:"리아",
+        src:require("../../Assets/itzyimgs/lia1.svg")
+    },
+    {
+        name:"아린",
+        src:require("../../Assets/ohmygirl_imgs/omgarin1.svg")
+    },
+    {
+        name:"유아",
+        src:require("../../Assets/ohmygirl_imgs/omgyooa1.svg")
+    },
+    {
+        name:"비니",
+        src:require("../../Assets/ohmygirl_imgs/omgbinnie1.svg")
+    },
 ]
 
 export default items;

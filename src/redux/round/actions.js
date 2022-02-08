40a16370c4878c657225cc9eb277ed57d6bc316f
mingyeongmo <mingyeongmp@naver.com>
2022-02-08 21:18:59 +0900

    action 설정

---
+++ src/redux/round/actions.js
@@ -0,0 +1,19 @@
+import { ROUND_OF_16, ROUND_OF_8, ROUND_OF_4 } from "./types";
+
+export const Choice_ROUND_OF_16 = () => {
+  return {
+    type: ROUND_OF_16,
+  };
+};
+
+export const Choice_ROUND_OF_8 = () => {
+  return {
+    type: ROUND_OF_8,
+  };
+};
+
+export const Choice_ROUND_OF_4 = () => {
+  return {
+    type: ROUND_OF_4,
+  };
+};

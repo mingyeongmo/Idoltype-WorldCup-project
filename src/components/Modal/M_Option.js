0f5f62a2629b85ba8111d2c11fb094e25f482182
mingyeongmo <mingyeongmp@naver.com>
2022-02-16 23:14:13 +0900

    선택에 따라 달라지는 round의값

--- src/components/Modal/M_Option.js
+++ src/components/Modal/M_Option.js
@@ -8,32 +8,27 @@ import {
 } from "../../redux/round/actions";
 
 const M_Option = ({
-  setRound,
   Choice_ROUND_OF_16,
   Choice_ROUND_OF_8,
   Choice_ROUND_OF_4,
 }) => {
-  const [value, setValue] = useState();
   const handleValue = (e) => {
-    setValue(e.target.value);
+    // setValue(e.target.value);
+    // console.log("hi");
+    // console.log(e.target.value);
+    const roundValue = e.target.value;
 
-    console.log(opValue);
-
-    if (opValue == "16") {
+    if (roundValue == "16") {
       console.log("I'm 16");
-      setRound(16);
-    } else if (opValue == "8") {
+      Choice_ROUND_OF_16();
+    } else if (roundValue == "8") {
       console.log("I'm 8");
-      setRound(8);
-      // Choice_ROUND_OF_8();
-    } else if (opValue == "4") {
+      Choice_ROUND_OF_8();
+    } else if (roundValue == "4") {
       console.log("I'm 4");
-      setRound(4);
-      // Choice_ROUND_OF_4();
+      Choice_ROUND_OF_4();
     }
   };
-
-  const opValue = value;
 
   return (
     <Modal_Select className="selected" onChange={handleValue}>
@@ -50,17 +45,24 @@ const M_Option = ({
   );
 };
 
-// const mapDispatchToProps = {
-//   Choice_ROUND_OF_16,
-//   Choice_ROUND_OF_8,
-//   Choice_ROUND_OF_4,
-// };
-const mapDispatchToProps = (dispatch) => {
+const mapStateToProps = ({ rounds }) => {
   return {
-    Choice_ROUND_OF_16: () => dispatch(Choice_ROUND_OF_16()),
-    Choice_ROUND_OF_8: () => dispatch(Choice_ROUND_OF_8()),
-    Choice_ROUND_OF_4: () => dispatch(Choice_ROUND_OF_4()),
+    round: rounds.round,
   };
 };
-// export default M_Option;
-export default connect(mapDispatchToProps)(M_Option);
+
+const mapDispatchToProps = {
+  Choice_ROUND_OF_16,
+  Choice_ROUND_OF_8,
+  Choice_ROUND_OF_4,
+};
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     Choice_ROUND_OF_16: () => dispatch(Choice_ROUND_OF_16()),
+//     Choice_ROUND_OF_8: () => dispatch(Choice_ROUND_OF_8()),
+//     Choice_ROUND_OF_4: () => dispatch(Choice_ROUND_OF_4()),
+//   };
+// };
+
+export default connect(mapStateToProps, mapDispatchToProps)(M_Option);
